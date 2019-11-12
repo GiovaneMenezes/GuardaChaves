74cebfec2354b05410a8b84a407976c175eac044
Giovane Menezes <giovane.menezes@live.com>
2019-11-12 00:24:13 -0300

    Improve directory and remove unnecessary storyboard file

---
+++ Prova Pratica iOS Cedro/AppSections/Login/View/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Prova Pratica iOS Cedro
+//
+//  Created by Giovane Silva de Menezes Cavalcante on 12/11/19.
+//  Copyright © 2019 GSMenezes. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewController {
+    
+}
---
+++ Prova Pratica iOS Cedro/AppSections/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewModel.swift
+//  GuardaChaves
+//
+//  Created by Giovane Silva de Menezes Cavalcante on 12/11/19.
+//  Copyright © 2019 GSMenezes. All rights reserved.
+//
+
+import Foundation
